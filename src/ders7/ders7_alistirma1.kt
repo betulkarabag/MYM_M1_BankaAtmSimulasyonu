bc68720c4efb8ac3947a15d6ad5bb5fde700a361
betulkarabag <betulkarabag3467@gmail.com>
2026-07-24 13:08:31 +0300

    Ders 7: Değişiklikler

--- src/ders7/ders7_alistirma1.kt
+++
@@ -1,9 +0,0 @@
-package ders7
-
-fun main() {
-     println("Bir sayı giriniz: ")
-    val sayi = readln().toInt()
-    for (i in 1..10){
-        println("$sayi x $i = ${sayi*i}")
-    }
-}
--- src/ders7/ders7_alistirma3.kt
+++
@@ -1,9 +0,0 @@
-package ders7
-
-fun main() {
-    var toplam = 0
-    for(i in 2..100 step 2){
-        toplam += i
-    }
-println(toplam)
-}
